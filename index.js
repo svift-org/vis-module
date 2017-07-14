e6724cc574e3078d9e550873bd2c75e7fb1d5843
sebastian-meier <contact@sebastianmeier.eu>
2017-07-15 00:57:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+* 
+* 
+* 
+*/
+
+SVIFT.vis = {};
+SVIFT.vis.base = (function (data, root) {
+ 
+  var module = {};
+
+  module.config = {
+    maxWidth : 4096,
+    maxHeight : 4096,
+    margin : {
+      top: 10,
+      right: 10,
+      bottom: 25,
+      left: 35
+    }
+  };
+
+  module.init = function () {
+    root.append('svg')
+      .style('width','100%')
+      .style('height','100%')
+      .attr('width', '100%')
+      .attr('height', '100%');
+
+    d3.select(window).on('resize', SVIFT.helper.debouncer(function(e){
+      module.resize();
+    }, 200));
+  };
+
+  module.resize = function () {
+    console.log('resize');
+  };
+
+  return module;
+});
