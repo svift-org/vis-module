5aabfc443692f5b57052e1c83186a12c5f0341a8
sebastian-meier <contact@sebastianmeier.eu>
2017-07-15 02:05:04 +0200

    bugfixing

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ SVIFT.vis.base = (function (data, root) {
 
   var module = {};
 
+  module.svg = null;
+
   module.config = {
     maxWidth : 4096,
     maxHeight : 4096,
     margin : {
+      //TODO: Margin should have a minimum and then be relative %
       top: 10,
       right: 10,
       bottom: 25,
@@ -21,19 +24,23 @@ SVIFT.vis.base = (function (data, root) {
   };
 
   module.init = function () {
-    root.append('svg')
-      .style('width','100%')
-      .style('height','100%')
+    module.svg = root.append('svg')
       .attr('width', '100%')
-      .attr('height', '100%');
+      .attr('height', '100%')
+      .append('g')
+        .attr('transform','translate('+module.config.margin.top+','+module.config.margin.left+')');
+
+    module.setup();
 
     d3.select(window).on('resize', SVIFT.helper.debouncer(function(e){
       module.resize();
     }, 200));
   };
 
+  module.setup = function () {
+  };
+
   module.resize = function () {
-    console.log('resize');
   };
 
   return module;
