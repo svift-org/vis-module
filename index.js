b96bf314a0c6bc290de26bc9677f4a06480f124e
sebastian-meier <contact@sebastianmeier.eu>
2017-07-17 15:23:20 +0200

    framebased animation

--- index.js
+++ index.js
@@ -19,12 +19,17 @@ SVIFT.vis.base = (function (data, container) {
     maxHeight : 4096,
     margin : {
       //TODO: Margin should have a minimum and then be relative %
-      top: 10,
-      right: 10,
-      bottom: 25,
-      left: 35
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 20
     }
   };
+
+  module.timeline = [
+    //Start and End are in Milliseconds, func receives a value between 0 and 1, obj is a container to store related info e.g. easing, etc.
+    //rect:{start:0, end:3000, func:module.drawRect, obj:{}}
+  ];
 
   module.init = function () {
     module.svg = module.container.append('svg')
@@ -33,6 +38,13 @@ SVIFT.vis.base = (function (data, container) {
 
     module.g = module.svg.append('g')
       .attr('transform','translate('+module.config.margin.top+','+module.config.margin.left+')');
+
+    for( var key in module.timeline ){
+      var tl = module.timeline[key];
+      if(module.playTime < tl.end){
+        module.playTime = tl.end;
+      }
+    }
 
     module.setup();
     module.resize();
@@ -46,6 +58,74 @@ SVIFT.vis.base = (function (data, container) {
   };
 
   module.resize = function () {
+    //Resize should consider height and width (e.g. Bootstrap 16-9 resizes height and width of embed elements)
+  };
+
+  module.draw = function (t) {
+    for( var key in module.timeline ){
+      var tl = module.timeline[key];
+      if(tl.start >= t && tl.end <= t){
+        tl.func((t-tl.start)/(tl.end-tl.start));
+      }
+    }
+  };
+
+  module.playHead = 0;
+  module.playState = false;
+  module.playTime = 0;
+  module.time = {
+    elapsed : 0,
+    fpsInterval : 1000 / 30, //30 fps
+    then : 0,
+    startTime : 0,
+    now : 0
+  };
+
+  module.play = function () {
+    if(module.time.then === 0){
+      module.time.then = module.time.startTime = Date.now();
+    }
+
+    if(module.playState){
+      module.playHead++;
+      if(module.playHead<=module.playTime){
+        if (typeof window !== 'undefined') {
+          //Standard Web Implementation
+          window.requestAnimationFrame(module.play);
+
+          module.time.now = Date.now();
+          module.time.elapsed = module.time.now - module.time.then;
+
+          if (module.time.elapsed > module.time.fpsInterval) {
+            module.time.then = module.time.now - (module.time.elapsed % module.time.fpsInterval);
+            module.draw(module.playHead);
+          }
+        }else{
+          //Node.JS rendering
+          module.draw(module.playHead);
+          setTimeout(module.play, 0);
+        }
+
+      }else{
+        module.pause();
+      }
+    }
+  };
+
+  module.start = function () {
+    module.playState  = true;
+    module.play();
+  };
+
+  module.pause = function () {
+    module.playState = false;
+  };
+
+  module.goto = function (t) {
+    module.pause();
+    if(t<=module.playTime){
+      module.draw(t);
+    }
   };
 
   return module;
