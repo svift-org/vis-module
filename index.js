52d7eef31d84b88eab206fef1c963243a6c1f7a0
hhack <hans.f.hack@googlemail.com>
2017-11-06 15:00:37 +0100

    added text

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ SVIFT.vis.base = (function (data, container) {
       right: 20,
       bottom: 20,
       left: 20
-    }
+    },
+    font:{
+      sizes:{
+        "1500": 25,
+        "1000": 20,
+        "0":16
+      },
+      default: "16px"
+    },
+    paddingTopText: 20
   };
 
   module.timeline = {
@@ -32,9 +41,19 @@ SVIFT.vis.base = (function (data, container) {
   };
 
   module.init = function () {
+
+    var screenWidth = module.container.node().offsetWidth;
+    var fontSize;
+    for( var key in module.config.font.sizes ){
+      if(screenWidth>=key){
+        fontSize = module.config.font.sizes[key]
+      }
+    }
+
     module.svg = module.container.append('svg')
       .attr('width', '100%')
       .attr('height', '100%')
+      .attr('font-size', fontSize) //font size match function goeas here
       //temporary testing for phantom rendering
       .style('background-color','#ffffff');
 
@@ -42,10 +61,51 @@ SVIFT.vis.base = (function (data, container) {
     module.g = module.svg.append('g')
       .attr('transform','translate('+module.config.margin.top+','+module.config.margin.left+')');
 
+
+    //Text Top
+    module.d3config.topTextWrapper = module.g.append("g")
+      .attr("text-anchor", "middle")
+      .attr("class", "title-wrapper" )
+      .attr("font-family", data.style.font)
+
+    module.d3config.titleMain = module.d3config.topTextWrapper.append("text")
+      .text(data.data.title)
+      .attr("font-size", "1.7em")
+      .attr("fill", data.style.color.main)
+
+    module.d3config.titleSub = module.d3config.topTextWrapper.append("text")
+      .text(data.data.subTitle)
+      .attr("font-size", "1em")
+      .attr("fill", data.style.color.second)
+
+    //Text Bottom
+    module.d3config.bottomTextWrapper = module.g.append("g")
+      .attr("font-family", data.style.font)
+
+    module.d3config.attribution = module.d3config.bottomTextWrapper
+      .append("text")
+      .text(data.data.attribution)
+      .attr("font-size", module.config.font.default)
+      .attr("fill", data.style.color.second)
+
+    module.d3config.source = module.d3config.bottomTextWrapper
+      .append("text")
+      .text(data.data.source)
+      .attr("font-size", module.config.font.default)
+      .attr("fill", data.style.color.second)
+
+    //Make a Viz Container
+    module.d3config.vizContainer = module.g
+      .append("g")
+      .attr("class", "viz-container")
+
+
     module.playHead = 0;
 
     module.setup();
+    module.resizeText()
     module.resize();
+    
 
     for( var key in module.timeline ){
       var tl = module.timeline[key];
@@ -61,7 +121,11 @@ SVIFT.vis.base = (function (data, container) {
     }
 
     d3.select(window).on('resize', SVIFT.helper.debouncer(function(e){
+
+      module.resizeText()
       module.resize();
+      
+
     }, 200));
   };
 
@@ -81,6 +145,48 @@ SVIFT.vis.base = (function (data, container) {
     //Resize should consider height and width (e.g. Bootstrap 16-9 resizes height and width of embed elements)
     //For some weird reason phantom resizes the page before every rendering, pay attention to this problem
   };
+
+  //function that resizes/positions the text
+  module.resizeText = function(){
+
+    var vizWidth = module.container.node().offsetWidth - module.config.margin.left - module.config.margin.right;
+    var vizHeight= module.container.node().offsetHeight - module.config.margin.top - module.config.margin.bottom;
+    var vizCenter = vizWidth/2;
+
+    module.d3config.titleMain
+      .attr("x", vizCenter)
+      .attr("y", function(){
+        module.d3config.titleMainHeight = this.getBBox().height
+        return module.d3config.titleMainHeight
+      })
+
+    module.d3config.titleSub
+      .attr("x", vizCenter)
+      .attr("y", function(){
+        return (module.d3config.titleMainHeight + this.getBBox().height + 10)
+      })
+
+    module.d3config.attribution
+      .attr("x", function(){
+        return vizWidth - this.getBBox().width
+      })
+      .attr("y", function(){
+        return vizHeight - this.getBBox().height
+      })
+
+    module.d3config.source
+      .attr("x", 0)
+      .attr("y", function(){
+        return vizHeight - this.getBBox().height
+      })
+
+
+    module.d3config.topTextHeight = module.d3config.topTextWrapper.node().getBBox().height + module.config.paddingTopText;
+    module.d3config.bottomTextHeight = module.d3config.bottomTextWrapper.node().getBBox().height ;
+
+    module.d3config.vizContainer
+      .attr('transform','translate(0,'+ module.d3config.topTextHeight +')')
+  }
 
   module.draw = function (t) {
     for( var key in module.timeline ){
